f2d3b4d14c5ce6773cdb059e49fc6583c8a7b9df
Taha Bhat <tahabhat47@hotmail.com>
2018-12-21 13:33:28 +0530

    subtraction feature added

--- Calculator/Class1.cs
+++ Calculator/Class1.cs
@@ -4,5 +4,13 @@ namespace Calculator
 {
     public class Class1
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
